e5340865cbf55c18c570abd014f9dcdea20d31c2
SohanaN <sohananaz01@gmail.com>
2026-03-05 00:23:20 +0600

    Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,6 +5,7 @@ export default function Banner() {
   return (
     <>
       <div className="grid md:grid-cols-2 gap-2 m-2">
+        {/* In-Progress */}
         <div className="relative w-full h-50 md:h-60 rounded-xl overflow-hidden shadow-lg flex flex-col items-center justify-center text-white text-center bg-linear-to-r from-[#632ee3e6] to-[#9f62f2e6]">
           <div
             className="absolute inset-y-0 left-0 w-1/2 z-10"
@@ -28,9 +29,11 @@ export default function Banner() {
           ></div>
           <div>
             <p className="text-2xl">In-Progress</p>
-            <p className="text-4xl font-semibold mt-2">0</p>
+            <p className="text-5xl font-semibold mt-2">0</p>
           </div>
         </div>
+
+        {/* Resolved */}
         <div className="relative w-full h-60 rounded-xl overflow-hidden shadow-lg flex flex-col items-center justify-center text-white text-center bg-linear-to-r from-[#54CF68] to-[#00827A]">
           <div
             className="absolute inset-y-0 left-0 w-1/2 z-10"
@@ -54,7 +57,7 @@ export default function Banner() {
           ></div>
           <div>
             <p className="text-2xl">Resolved</p>
-            <p className="text-4xl font-semibold mt-2">0</p>
+            <p className="text-5xl font-semibold mt-2">0</p>
           </div>
         </div>
       </div>
